f412e1eb15eb5c9cbd33631bf682f1528420381a
Sunil Kavatkar <68806268+sunilkavatkar@users.noreply.github.com>
2021-03-17 21:19:53 +0530

    Add files via upload

---
+++ carpred.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 17 09:41:33 2021
+
+@author: sunil
+"""
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sn
+
